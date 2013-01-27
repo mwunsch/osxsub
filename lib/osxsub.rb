9f71cbd1ab9aeb0b95f8a39df9cc463e89a9dd99
Mark Wunsch <mark@markwunsch.com>
2013-01-27 01:03:32 -0500

    First spike: Low level classes/errors for working with PlistBuddy
    Programmatic way of building up plists of substitutions

--- lib/osxsub.rb
+++ lib/osxsub.rb
@@ -1,29 +1,7 @@
+require 'osxsub/version'
+require 'osxsub/substitution'
+require 'osxsub/plist_buddy'
+require 'osxsub/ns_user_replacement_items'
+
 module OsxSub
-  class Substitution
-    attr_reader :replace, :with
-
-    def initialize(replace, with, on = true)
-      @replace = replace
-      @with = with
-      @on = on
-    end
-
-    def on?
-      return false if @on.is_a? String and @on.to_i <= 0
-      true & @on
-    end
-
-    def sub(string)
-      string.gsub(replace, with)
-    end
-  end
-
-  class NSUserReplacementItems
-    def self.print
-      command = %q{/usr/libexec/PlistBuddy -x -c "Print NSUserReplacementItems" ~/Library/Preferences/.GlobalPreferences.plist 2> /dev/null}
-      xml = `#{command}`
-      abort("There was an error: #{$?}") if $?.exitstatus > 0 or xml.empty?
-      xml
-    end
-  end
 end
---
+++ lib/osxsub/version.rb
@@ -0,0 +1,3 @@
+module OsxSub
+  VERSION = "0.1.0.snapshot"
+end
